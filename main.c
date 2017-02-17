18830ad7ed630450a920ce07d4da4d37ad3cb53b
Axel Viala <ENTER YOUR EMAIL HERE>
2017-02-17 01:14:52 +0100

    Removed useless field in slices, errors, fork.
    Sould work.

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 #include <assert.h>
 
+#include <wait.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -19,37 +20,41 @@
 
 extern char **environ;
 
+/*
+ * Little slice struct for avoiding copy and
+ * getting cool with string usage! all inline.
+ */
 typedef struct _str_slice {
-  const char* start;
-  const char* end;
-  const uint32_t len; // end - start len of slice.
+  const char* data;
+  const uint32_t len; // end - data len of slice.
 } str_slice;
 
-str_slice slice_at(const char* str, const char c) {
-  const char* end = strchr(str, c);
+inline
+str_slice slice_at(const char* data, const char c) {
+  const char* end = strchr(data, c);
   return (const str_slice) {
-    str,
-    end,
-    end - str
-  };
+    data,
+      end - data
+      };
 }
 
+inline
 str_slice slice_next(const str_slice slice, const char c) {
-  const char* start = slice.end + 1;
-  const char* end = strchr(start, c);
+  const char* data = slice.data + slice.len + 1; // skip c
+  const char* end = strchr(data, c);
   if (end != NULL) {
     return (str_slice) {
-      start,
-      end,
-      end - start
+      data,
+      end - data
     };
   } else {
-    return (str_slice) {0, 0, 0};
+    return (str_slice) {0, 0};
   }
 }
 
+inline
 bool slice_empty(const str_slice s) {
-  return s.len == 0 && s.end == 0 && s.start == 0;
+  return s.len == 0;
 }
 
 const char* build_full_path(const char* progname, const str_slice slice) {
@@ -58,7 +63,7 @@ const char* build_full_path(const char* progname, const str_slice slice) {
   char *full_path = malloc(sizeof(char*) * full_path_size);
 
   full_path[full_path_size] = '\0';
-  strncpy(full_path, slice.start, slice.len);
+  strncpy(full_path, slice.data, slice.len);
   full_path[slice.len] = '/';
   strcpy(full_path + slice.len + 1, progname);
 
@@ -72,15 +77,26 @@ char const* find_proper_path(const char* progname, const char* paths) {
     const char *full_path = build_full_path(progname, slice);
     struct stat file_stat = {};
     if (stat(full_path, &file_stat) == -1) {
-      perror("stat error:");
+      // perror("stat error"); It's OK if file not exist.
     } else {
-      return full_path;
-      break; // Fine we have our executable exit loop.
+      if ((file_stat.st_mode & S_IXUSR)
+          || (file_stat.st_mode & S_IXGRP)
+          || (file_stat.st_mode & S_IXGRP)) {
+        return full_path;
+        break; // Fine we have our executable exit loop.
+      }
     }
   }
+  // Found nothing :(
   return NULL;
 }
 
+/* 
+ * TODO: - relative path > $PATHS
+ *       - noWait &
+ *       - fg/bg?
+ *
+ */
 int main(int argc, char *argv[]) {
   // Try to lookup program in PATH.
   if (argc > 1) {
@@ -88,12 +104,24 @@ int main(int argc, char *argv[]) {
     assert(paths);
     const char* progname = argv[1];
     const char *full_path = find_proper_path(progname, paths);
-    execve(full_path, &argv[1], environ);
+    if (full_path != NULL) {
+      // Test if wait or not.
+      if (true) {
+        // Managing real work issues EAGAIN ENOMEM ENOSYS
+        int status; // Init when child exit.
+        const pid_t pid = fork();
+        if (pid < 0) {
+          perror("fork failed");
+        } else if (pid > 0) {
+          waitpid(pid, &status, 0);
+        } else {
+          execve(full_path, &argv[1], environ);
+        }
+      }
 
-
-
-    free((void *)full_path); // lifetime dans ce block.
-    free((void *)paths); // lifetime dans ce block.
+      free((void *)full_path); // lifetime dans ce block.
+      free((void *)paths); // lifetime dans ce block.
+    }
   }
   return EXIT_SUCCESS;
 }
